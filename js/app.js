4002e350f2e0d6566a3b878a22a69e06ea40b402
rahuldev <rahuldev.w@gmail.com>
2021-02-13 11:26:20 -0800

    full page scroll added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
+//---------Fill Page Scroll---------------
+if($('#fullpage').length > 0){
+	var myFullpage = new fullpage('#fullpage', {
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 700,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: false,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		bigSectionsDestination: null,
+
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: false,
+		verticalCentered: true,
+		paddingTop: '150px',
+		//paddingBottom: '10px',
+		fixedElements: '#header',
+		responsiveWidth: 1024,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+		//Custom selectors
+		sectionSelector: '.section',
+
+	});
+}
+
+
+
 $("[data-trigger]").on("click", function(e){
     e.preventDefault();
     e.stopPropagation();
