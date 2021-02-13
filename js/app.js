b695530ff36e42cb278e3fe5897fa5dfcb031089
rahuldev <rahuldev.w@gmail.com>
2021-02-13 15:43:01 -0800

    responsive changes update

--- js/app.js
+++ js/app.js
@@ -114,30 +114,30 @@ $('.testimonial-slider').slick({
 			{
 				breakpoint: 1600,
 				settings: {
-				  slidesToShow: 4,
-				  slidesToScroll: 4,
+				  slidesToShow: 5,
+				  slidesToScroll: 5,
 				}
 			  },
 			{
 				breakpoint: 1440,
 				settings: {
-				  slidesToShow: 3,
-				  slidesToScroll: 3,
+				  slidesToShow: 4,
+				  slidesToScroll: 4,
 				}
 			  },
 			  {
 				breakpoint: 1366,
 				settings: {
-				  slidesToShow: 3,
-				  slidesToScroll: 3,
+				  slidesToShow: 4,
+				  slidesToScroll: 4,
 
 				}
 			  },
 			{
 			  breakpoint: 1024,
 			  settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
+				slidesToShow: 2,
+				slidesToScroll: 2,
 			  }
 			},
 			{
@@ -150,8 +150,8 @@ $('.testimonial-slider').slick({
 			{
 			  breakpoint: 480,
 			  settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: 1,
+				slidesToScroll: 1
 			  }
 			},
 			{
