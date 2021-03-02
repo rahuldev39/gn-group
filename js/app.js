c9f0fd60a8db8f7f2c2d05df54993e69cf9a269d
rahuldev <rahuldev.w@gmail.com>
2021-03-01 19:43:21 -0800

    new changes update

--- js/app.js
+++ js/app.js
@@ -257,14 +257,13 @@ $('.testimonial-slider').slick({
             
         });
 
-        $('.companies-slider').slick({
+        $('.companies-slider1').slick({
 			centerMode: true,
             slidesToScroll: 6,
             arrows: false,
             dots: false,
             autoplay:true,
             infinite: true,
-			rows: 2,
             responsive: [
                 {
                     breakpoint: 1600,
@@ -327,3 +326,74 @@ $('.testimonial-slider').slick({
               ]
                 
             });
+
+			$('.companies-slider2').slick({
+				centerMode: true,
+				slidesToScroll: 6,
+				arrows: false,
+				dots: false,
+				autoplay:true,
+				infinite: true,
+				rtl: true,
+				responsive: [
+					{
+						breakpoint: 1600,
+						settings: {
+						  slidesToShow: 6,
+						  slidesToScroll: 6,
+						  centerPadding: '60px',
+						}
+					  },
+					{
+						breakpoint: 1440,
+						settings: {
+						  slidesToShow: 5,
+						  slidesToScroll: 5,
+						  centerPadding: '60px',
+						}
+					  },
+					  {
+						breakpoint: 1366,
+						settings: {
+						  slidesToShow: 5,
+						  slidesToScroll: 5,
+						  centerPadding: '60px',
+		
+						}
+					  },
+					{
+					  breakpoint: 1024,
+					  settings: {
+						slidesToShow: 4,
+						slidesToScroll: 4,
+						centerPadding: '40px',
+					  }
+					},
+					{
+					  breakpoint: 600,
+					  settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+						centerPadding: '30px',
+					  }
+					},
+					{
+					  breakpoint: 480,
+					  settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						centerPadding: '0px',
+	
+					  }
+					},
+					{
+						breakpoint: 320,
+						settings: {
+						  slidesToShow: 1,
+						  slidesToScroll: 1,
+						  centerPadding: '0px',
+						}
+					  }
+				  ]
+					
+				});
