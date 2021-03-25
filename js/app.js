383e0b6a9e8a818e53915961ce1e09ea9ba0794e
rahuldev <rahuldev.w@gmail.com>
2021-03-25 08:59:44 +0530

    career page added

--- js/app.js
+++ js/app.js
@@ -427,4 +427,34 @@ $('.testimonial-slider').slick({
 		});
 
 
+  //----------Career  Filter-------------
+
+  $(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+
+		
+
+			$(".filter-button").removeClass("active");
+	
+			$(this).addClass("active");
+    });
+
+
+		});
 		
